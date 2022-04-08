f13a8bb00fb2574be76e3ff9f7d80af64c745b48
blowekamp <blowekamp@bots.github.com>
2022-04-08 00:10:53 +0000

    Automatic Update Fri Apr  8 00:10:53 UTC 2022

--- search_config.php
+++ search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Thu Apr 7 2022 00:10:36 for ITK by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.17 ",
+  'logo' => "Generated on Fri Apr 8 2022 00:10:50 for ITK by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.17 ",
 );
 
 ?>
